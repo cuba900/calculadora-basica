25037de75a60a3e73f112c7b59333e2ebf4bb2c7
cuba900 <armandobaro@gmail.com>
2019-10-06 00:20:54 -0300

    update06102019

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function calcula(operacion){
-    let operando1 = document.calc.operando1.value
-    let operando2 = document.calc.operando2.value 
-    let result = eval(operando1 + operacion + operando2)
+function calcula(operacion) {
+    let operando1=document.calc.operando1.value
+    let operando2=document.calc.operando2.value 
+    let result=eval(operando1 + operacion + operando2)
     document.calc.resultado.value = result
 }
+
+function clearcalc(e) {
+    e.preventDefault();
+    $("#calc")[0].reset()
+}
+    
